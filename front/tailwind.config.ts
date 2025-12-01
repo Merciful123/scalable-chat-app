299bf0bbbd5ef5f9d9e5e68118cd7fc723332559
Aamir Raza <raza949597@gmail.com>
2025-12-02 01:14:43 +0530

    minor fix in tailwind config, deploy,

--- front/tailwind.config.ts
+++ front/tailwind.config.ts
@@ -5,7 +5,7 @@ import tailwindcssAnimate from "tailwindcss-animate";
 
 
 const config = {
-  darkMode: ["class"],
+  darkMode: "class",
   content: [
     './pages/**/*.{ts,tsx}',
     './components/**/*.{ts,tsx}',
@@ -77,7 +77,8 @@ const config = {
         "accordion-up": "accordion-up 0.2s ease-out",
       },
       fontFamily : {
-        sans:[ "var(--font-sans)", ...fontFamily.sans]
+        //@ts-expect-error
+        sans:[ "var(--font-sans)", ...fontFamily.san]
       }
     },
 
