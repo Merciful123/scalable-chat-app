8b44d9d1cba5acb2d21782a06d9adbe7a804a073
Aamir Raza <raza949597@gmail.com>
2025-12-02 02:27:45 +0530

    minor fix in url, deploy

--- front/src/lib/env.ts
+++ front/src/lib/env.ts
@@ -1,7 +1,14 @@
 class Env {
-    static APP_URL : string = process.env.NEXT_PUBLIC_APP_URL as string;
-    static BACKEND_URL: string = process.env.NEXT_PUBLIC_BACKEND_URL as string;
+    static APP_URL : string = process.env.NEXT_PUBLIC_APP_URL ?? "";
+    static BACKEND_URL: string = process.env.NEXT_PUBLIC_BACKEND_URL ?? "";
+
+      // Optional: helper to check if envs are missing
+    static validate() {
+        if (!this.APP_URL) throw new Error("NEXT_PUBLIC_APP_URL is not defined");
+        if (!this.BACKEND_URL) throw new Error("NEXT_PUBLIC_BACKEND_URL is not defined");
+  }
 }
 
+Env.validate();
 
 export default Env;
