2b1fd2ff25a99fd7571b65560499c4df8f5ee575
Aamir Raza <raza949597@gmail.com>
2025-12-02 02:49:28 +0530

    minor fix, deploy

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -7,6 +7,7 @@ import { getServerSession } from "next-auth";
 import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
 
 export default async function Home() {
+  
   const session:CustomSession | null = await getServerSession(authOptions)
   
   return (
