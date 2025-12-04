f1bcbe65a9b4bcf280e00e4de392ec70bb89d9c9
Aamir Raza <raza949597@gmail.com>
2025-12-04 19:38:22 +0530

    minor fix, url issue

--- front/src/middleware.ts
+++ front/src/middleware.ts
@@ -1,35 +1,6 @@
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+export {default} from "next-auth/middleware";
 
-export default withAuth(
-  function middleware(req) {
-    const url = req.nextUrl;
-
-    // Determine environment
-    const host = req.headers.get("host") || "";
-    const proto = req.headers.get("x-forwarded-proto") || "";
-
-    const isLocalhost = host.includes("localhost") || host.includes("127.0.0.1");
-
-    /**
-     *  Force HTTPS ONLY on production (Render)
-     * Localhost MUST remain HTTP, otherwise Chrome throws:
-     * ERR_SSL_PROTOCOL_ERROR
-     */
-    if (!isLocalhost && proto !== "http") {
-      url.protocol = "https:";
-      return NextResponse.redirect(url);
-    }
-
-    return NextResponse.next();
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => !!token, // Require authentication
-    },
-  }
-);
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
-};
+    matcher: ["/dashboard"],
+}
